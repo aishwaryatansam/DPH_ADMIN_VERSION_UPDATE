9c2f396c919f360b45218ee231a49257d09330b4
aishwaryatansam <aishwaryatansam@gmail.com>
2025-11-24 10:23:36 +0530

    feature/scheme_fix

--- App/Http/Controllers/Admin/SchemeController.php
+++ App/Http/Controllers/Admin/SchemeController.php
@@ -224,7 +224,6 @@ public function create(Request $request)
     public function attributes() {
         return [];
     }
-
     public function listScheme(Request $request) {
         $validator = Validator::make($request->all(),[
             'program_id' => 'required|exists:programs,id,status,'._active(),
