c45ce15fea07c9c851e0f30d8f5138d257f5c29b
aishwaryatansam <aishwaryatansam@gmail.com>
2025-12-04 15:20:02 +0530

    page url

--- resources/views/admin/program-details/list.blade.php
+++ resources/views/admin/program-details/list.blade.php
@@ -50,25 +50,7 @@
                             <div class="table-responsive">
                                  <table id="add-row" class="display table table-striped table-hover"
                                             style="width:100%">
-                                    {{-- <form method="GET" action="{{ url('/programdetails') }}" class="mb-3">
-    <div class="row align-items-center">
-        <div class="col-auto">
-            <label for="pageLength" class="me-2 mb-0">Show</label>
-            <select name="pageLength" id="pageLength" class="form-select w-auto" onchange="this.form.submit()">
-                @foreach(getPageLenthArr() as $pageLength)
-                    <option value="{{ $pageLength }}" {{ request('pageLength', 10) == $pageLength ? 'selected' : '' }}>
-                        {{ $pageLength }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <div class="col-auto ms-auto">
-            <label for="search">Search:</label>
-            <input type="search" name="search" id="search" value="{{ request('search') }}">
-            <button type="submit">Go</button>
-        </div>
-    </div>
-</form> --}}                                                        <form method="GET" action="{{ url('/programdetails') }}" class="mb-3"> 
+                                                                           <form method="GET" action="{{ url('/programdetails') }}" class="mb-3"> 
 
     <input type="hidden" name="programdetails" value="{{ request('programdetails') }}">
     <div class="d-flex justify-content-between align-items-center">
